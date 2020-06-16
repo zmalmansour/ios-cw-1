83c03a0a7f48994263aa5027733bf44c49bbfd35
zmalmansour <zmalmansourrr@gmail.com>
2020-06-16 12:06:28 +0300

    CW1 fork1

---
+++ CW1-zahraa almansour237.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let name = "zahraa"
+
+var age = 18
+
+var height = 164
+
+var hobby1 = "coding"
+
+var hobby2 = "playing oud"
+
+print(name)
+
+print(age,height)
+
+print("my hobbies are \(hobby1) and \(hobby2)")
