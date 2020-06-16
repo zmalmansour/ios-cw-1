7f8aa15822ad00cf024679148fadb5a5da9a0767
zmalmansour <zmalmansourrr@gmail.com>
2020-06-16 13:35:38 +0300

    Update Contents.swift

--- CW1-zahraa almansour237.playground/Contents.swift
+++ CW1-zahraa almansour237.playground/Contents.swift
@@ -15,3 +15,36 @@ print(name)
 print(age,height)
 
 print("my hobbies are \(hobby1) and \(hobby2)")
+
+let friendName = "fatima"
+
+var friendAge = 18
+
+var friendHeight = 161
+
+var friendHobby1 = "interior designing"
+
+var friendHobby2 = "cooking"
+
+age-friendAge
+
+// comparing
+
+name == friendName
+name != friendName
+
+age > friendAge
+age < friendAge
+age == friendAge
+age != friendAge
+
+height == friendHeight
+height != friendHeight
+
+hobby1 == friendHobby1
+hobby1 != friendHobby1
+hobby2 == friendHobby2
+hobby2 != friendHobby2
+
+
+
